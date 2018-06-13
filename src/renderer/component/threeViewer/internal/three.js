3d1e79e9fa768f9a5fe1d596142af2fcf424f0e0
btzr-io <btzr.io@gmail.com>
2018-06-12 22:45:40 -0600

    fix import issues

--- src/renderer/component/threeViewer/internal/three.js
+++ src/renderer/component/threeViewer/internal/three.js
@@ -1,12 +1,10 @@
 import * as THREE from 'three';
 
-import 'three/examples/js/controls/OrbitControls';
-import 'three/examples/js/loaders/OBJLoader';
-import 'three/examples/js/loaders/STLLoader';
-
 // Currently it's not possible to import the files within the "examples/js" directory.
 // Fix: https://github.com/mrdoob/three.js/issues/9562#issuecomment-383390251
-
 global.THREE = THREE;
+require('three/examples/js/controls/OrbitControls');
+require('three/examples/js/loaders/OBJLoader');
+require('three/examples/js/loaders/STLLoader');
 
 module.exports = global.THREE;
